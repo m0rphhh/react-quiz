078f80e2734b97345973807cf5214633641b22eb
m0rphhh <62161639+m0rphhh@users.noreply.github.com>
2020-07-26 20:20:55 +1000

    Initial commit

---
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -0,0 +1,34 @@
+import React from "react";
+import classes from "./FinishedQuiz.css"
+
+const FinishedQuiz = props => {
+    return (
+        <div className={classes.FinishedQuiz }>
+            <ul>
+                {props.quiz.map((quizItem, index) => {
+                    const cls = [
+                        'fa',
+                        props.results[quizItem.id] === 'error' ? 'fa-times' : 'fa-check',
+                        classes[props.results[quizItem.id]]
+                    ]
+                     return (
+                         <li
+                             key={index}
+                         >
+                            <strong>{index + 1}</strong>.&nbsp;
+                             {quizItem.question}
+                             <i className={cls.join(' ')} />
+                         </li>
+                     )
+                })}
+            </ul>
+
+            <p>Правильно 4 из 10</p>
+            <div>
+                <button>Повторить</button>
+            </div>
+        </div>
+    )
+}
+
+export default FinishedQuiz
